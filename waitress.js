60a4b0b878686595829effde085d83177217a34b
Bjoern Zinssmeister <bjoern@userfox.com>
2013-11-22 20:14:44 -0800

    Tabs to spaces

--- waitress.js
+++ waitress.js
@@ -1,32 +1,32 @@
-//    Waitress.js 0.0.1
+//  Waitress.js 0.0.1
 
-//    (c) 2013 Bjoern Zinssmeister
-//		Waitress may be freely distributed under the MIT license.
-//		twitter.com/zinssmeister
+//  (c) 2013 Bjoern Zinssmeister
+//  Waitress may be freely distributed under the MIT license.
+//  twitter.com/zinssmeister
 //
 (function(){
-	Waitress = function(){
-		this.completed = new Array();
-		this.add = function(task){
-		  this.completed.push(task);
-		};
-		this.when = function(tasks, callback){
-		  this.callback = callback;
-		  var self = this;
-		  this.i = setInterval(function(){
-		    self.listenForAll(tasks);
-		  },50);
-		};
-		this.listenForAll = function(tasks){
-		  var notFound = 0;
-		  for(var i = 0; i < tasks.length; i++){
-		    if(this.completed.indexOf(tasks[i]) == -1) notFound++;
-		  }
-		  if(notFound == 0) this.ready();
-		};
-		this.ready = function(){
-		  clearInterval(this.i);
-		  this.callback();
-		};
-	};
+  Waitress = function(){
+    this.completed = new Array();
+    this.add = function(task){
+      this.completed.push(task);
+    };
+    this.when = function(tasks, callback){
+      this.callback = callback;
+      var self = this;
+      this.i = setInterval(function(){
+        self.listenForAll(tasks);
+      },50);
+    };
+    this.listenForAll = function(tasks){
+      var notFound = 0;
+      for(var i = 0; i < tasks.length; i++){
+        if(this.completed.indexOf(tasks[i]) == -1) notFound++;
+      }
+      if(notFound == 0) this.ready();
+    };
+    this.ready = function(){
+      clearInterval(this.i);
+      this.callback();
+    };
+  };
 }).call(this);
